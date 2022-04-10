2a8ad9c4e62e2446d1afc3c149651e0fa5dbb0c5
ben10nyson <mittalan@tcd.ie>
2022-04-10 09:36:39 +0100

    Plot_class changes
    Fixed some legends, titles, spacing between the subplots; still to see the x-axis and y-axis values and stuff, will do that in a separate one; also, Miguel reminded about the runs thing

--- Plot_Class.py
+++ Plot_Class.py
@@ -32,14 +32,14 @@ def plot1():
     # fig, ((ax1, ax3, ax5), (ax2, ax4, ax6)) = plt.subplots(2, 3, figsize=(10, 10))
     fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(10, 10))
 
-
     # DFS plotting
+    # print(genetic_rows[0])
     ax1.plot(rows[1], color="magenta", label="Deep First Search")
-    ax1.plot(mcts_rows[1], color="blue", label="MCTS First Search")
+    ax1.plot(mcts_rows[1], color="blue", label="MCTS")
     ax1.plot(genetic_rows[0], genetic_rows[1], color="orange", label="Genetic")
     ax1.set_xlabel("Time Taken (ms)")
     ax1.set_ylabel("Score")
-    ax1.set_title("DFS: Time Taken vs Score")
+    ax1.set_title("Time Taken vs Score")
     xticks = ax1.get_xticks()
     ax1.set_xticks(xticks[::len(xticks) // 5])
     ax1.xaxis.set_major_formatter(FormatStrFormatter('%.2f'))
@@ -49,11 +49,11 @@ def plot1():
 
 
     ax2.plot(rows[2], color="magenta", label="Deep First Search")
-    ax2.plot(mcts_rows[2], color="blue", label="Deep First Search")
+    ax2.plot(mcts_rows[2], color="blue", label="MCTS")
     ax2.plot(genetic_rows[0], genetic_rows[2], color="orange", label="Genetic")
     ax2.set_xlabel("Time Taken (ms)")
     ax2.set_ylabel("Lines Cleared")
-    ax2.set_title("DFS: Time Taken vs Lines Cleared")
+    ax2.set_title("Time Taken vs Lines Cleared")
     xticks = ax2.get_xticks()
     ax2.set_xticks(xticks[::len(xticks) // 5])
     ax2.xaxis.set_major_formatter(FormatStrFormatter('%.2f'))
@@ -88,13 +88,16 @@ def plot2():
             genetic_rows.append(row)
 
     fig, ((ax1, ax3, ax5), (ax2, ax4, ax6)) = plt.subplots(2, 3, figsize=(10, 10))
+    # fig.tight_layout()
+    fig.subplots_adjust(left=None, bottom=None, right=None, top=None, wspace=0.3, hspace=0.5)
+
 
 
     # DFS plotting
     ax1.plot(rows[0], rows[1], color="magenta", label="Deep First Search")
     ax1.set_xlabel("Time Taken (ms)")
     ax1.set_ylabel("Score")
-    ax1.set_title("DFS: Time Taken vs Score")
+    ax1.set_title("DFS: \n Time Taken vs Score")
     xticks = ax1.get_xticks()
     ax1.set_xticks(xticks[::len(xticks) // 5])
     ax1.xaxis.set_major_formatter(FormatStrFormatter('%.2f'))
@@ -106,7 +109,7 @@ def plot2():
     ax2.plot(rows[0], rows[2], color="magenta", label="Deep First Search")
     ax2.set_xlabel("Time Taken (ms)")
     ax2.set_ylabel("Lines Cleared")
-    ax2.set_title("DFS: Time Taken vs Lines Cleared")
+    ax2.set_title("DFS: \n Time Taken vs Lines Cleared")
     xticks = ax2.get_xticks()
     ax2.set_xticks(xticks[::len(xticks) // 5])
     ax2.xaxis.set_major_formatter(FormatStrFormatter('%.2f'))
@@ -114,7 +117,7 @@ def plot2():
     ax2.set_yticks(yticks[::len(yticks) // 8])
     ax2.legend()
 
-    ax3.plot(mcts_rows[0], mcts_rows[1], color="blue", label="MCTS First Search")
+    ax3.plot(mcts_rows[0], mcts_rows[1], color="blue", label="MCTS")
     ax3.set_xlabel("Time Taken (ms)")
     ax3.set_ylabel("Score")
     xticks = ax3.get_xticks()
@@ -122,11 +125,11 @@ def plot2():
     ax3.xaxis.set_major_formatter(FormatStrFormatter('%.2f'))
     yticks = ax3.get_yticks()
     ax3.set_yticks(yticks[::len(yticks) // 8])
-    ax3.set_title("MCTS: Time Taken vs Score")
+    ax3.set_title("MCTS: \n Time Taken vs Score")
     ax3.legend()
 
 
-    ax4.plot(mcts_rows[0], mcts_rows[2], color="blue", label="Deep First Search")
+    ax4.plot(mcts_rows[0], mcts_rows[2], color="blue", label="MCTS")
     ax4.set_xlabel("Time Taken (ms)")
     ax4.set_ylabel("Lines Cleared")
     xticks = ax4.get_xticks()
@@ -134,7 +137,7 @@ def plot2():
     ax4.xaxis.set_major_formatter(FormatStrFormatter('%.2f'))
     yticks = ax4.get_yticks()
     ax4.set_yticks(yticks[::len(yticks) // 8])
-    ax4.set_title("MCTS: Time Taken vs Score")
+    ax4.set_title("MCTS: \n Time Taken vs Lines Cleared")
     ax4.legend()
 
     ax5.plot(genetic_rows[0], genetic_rows[1], color="orange", label="Genetic")
@@ -145,7 +148,7 @@ def plot2():
     ax5.xaxis.set_major_formatter(FormatStrFormatter('%.2f'))
     yticks = ax3.get_yticks()
     ax5.set_yticks(yticks[::len(yticks) // 8])
-    ax5.set_title("Genetic: Time Taken vs Score")
+    ax5.set_title("Genetic: \n Time Taken vs Score")
     ax5.legend()
 
 
@@ -157,7 +160,7 @@ def plot2():
     ax6.xaxis.set_major_formatter(FormatStrFormatter('%.2f'))
     yticks = ax4.get_yticks()
     ax6.set_yticks(yticks[::len(yticks) // 8])
-    ax6.set_title("Genetic: Time Taken vs Score")
+    ax6.set_title("Genetic: \n Time Taken vs Lines CLeared")
     ax6.legend()
 
     plt.show()
